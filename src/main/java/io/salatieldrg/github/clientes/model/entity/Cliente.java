08ae26dd1c030a6837e8d9d95713f94d58bdb40c
Salatiel dos Reis <salatieldrg@gmail.com>
2022-12-06 10:31:40 -0300

    Mapeamento da entidade Cliente

---
+++ src/main/java/io/salatieldrg/github/clientes/model/entity/Cliente.java
@@ -0,0 +1,25 @@
+package io.salatieldrg.github.clientes.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+
+}
