4d4e55f0b5ff804df6a8b805a610c408c5bf5378
Salatiel dos Reis <salatieldrg@gmail.com>
2022-12-06 12:29:26 -0300

    Adição de cliente via requisição Rest

---
+++ src/main/java/io/salatieldrg/github/clientes/rest/ClienteController.java
@@ -0,0 +1,25 @@
+package io.salatieldrg.github.clientes.rest;
+
+import io.salatieldrg.github.clientes.model.entity.Cliente;
+import io.salatieldrg.github.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+}
